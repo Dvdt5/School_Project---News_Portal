35e04db91dadcff79c1966bccdd30edb553cd1ff
Lenovo <Lenovo@LAPTOP-RPLSVPQ4>
2024-11-07 20:15:54 +0300

    Add project files.

---
+++ School_Project---News_Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace School_Project___News_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
